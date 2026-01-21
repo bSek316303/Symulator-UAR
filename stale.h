872851315134ea308f4c6cdf5492c0e964f3293a
bSek316303 <bs316303@student.polsl.pl>
2026-01-21 12:08:59 +0100

    1

--- stale.h
+++ stale.h
@@ -27,8 +27,8 @@ constexpr int POCZ_ZAKRES_X = 10;
 
 // Stabilne dane
 constexpr double POCZ_KP = 1.0;
-constexpr double POCZ_TI = 0.1;
-constexpr double POCZ_TD = 0.05;
+constexpr double POCZ_TI = 4.0;
+constexpr double POCZ_TD = 1.05;
 constexpr int POCZ_LICZ_CALKE = 0;
 const std::vector<double> POCZ_A = {0.7};
 const std::vector<double> POCZ_B = {0.2};
@@ -37,11 +37,11 @@ constexpr int POCZ_SZUM = 0;
 
 // Generator
 
-constexpr double POCZ_AMP = 5.0;
-constexpr double POCZ_S = 1.0;
+constexpr double POCZ_AMP = 2.0;
+constexpr double POCZ_S = 0.0;
 constexpr double POCZ_P = 1.0;
 constexpr int POCZ_SYGNAL = 0;
-constexpr double POCZ_OKRES = 1.0;
+constexpr double POCZ_OKRES = 4.0;
 constexpr int POCZ_TAKTOWANIE = 200;
 constexpr int POCZ_ZAKRES_X = 10;
 
